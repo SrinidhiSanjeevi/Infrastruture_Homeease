67769335af2cd038d979bf7e80a37bf98a73a3cd
SrinidhiSanjeevi <srinidhip.s@zapcg.com>
2026-08-30 08:50:50 +0000

    Allow inbound HTTP (80) on AKS subnet NSG for ingress-nginx LoadBalancer

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -59,6 +59,17 @@ resource "azurerm_network_security_group" "aks" {
     source_address_prefix      = "Internet"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "AllowHTTPInbound"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
 
   tags = var.tags
 }
